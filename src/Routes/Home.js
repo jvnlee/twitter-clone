29d607ec4966260b04c97c0ddd65990a9c56a44a
jvnlee <lhjun1028@naver.com>
2021-09-01 21:50:13 +0900

    Cloning Twitter with React and Firebase

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,13 @@
 import Jweet from "Components/Jweet";
-import { dbService } from "myFirebase";
-import { useEffect, useState } from "react";
+import { dbService, storageService } from "myFirebase";
+import { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = ({ userObj }) => {
   const [jweet, setJweet] = useState("");
   const [jweets, setJweets] = useState([]);
+  const [attachment, setAttachment] = useState();
+  const fileInput = useRef();
 
   useEffect(() => {
     dbService
@@ -21,12 +24,14 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("jweets").add({
-      text: jweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setJweet("");
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    await fileRef.putString(attachment, "data_url");
+    // await dbService.collection("jweets").add({
+    //   text: jweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
+    // setJweet("");
   };
 
   const onChange = (event) => {
@@ -34,6 +39,25 @@ const Home = ({ userObj }) => {
       target: { value },
     } = event;
     setJweet(value);
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const attachedFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(attachedFile);
+  };
+
+  const onClearAttachment = () => {
+    fileInput.current.value = null;
   };
 
   return (
@@ -46,7 +70,16 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Jweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment} ref={fileInput}>
+              Clear
+            </button>
+          </div>
+        )}
       </form>
       <div>
         {jweets.map((jweet) => (
